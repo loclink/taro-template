ab86ecb7b62b0ee57a2f109cc836682f1a3a1e94
loclink <loclink@163.com>
2023-04-21 00:30:29 +0800

    test: 测试样式作用域

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { View } from '@tarojs/components';
+import React, { memo } from 'react';
+import './index.scss';
+
+const Home: React.FC = memo(() => {
+  return <View className='test'>Home</View>;
+});
+
+export default Home;
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,9 @@
+import { View } from '@tarojs/components';
+import React, { memo } from 'react';
+import './index.scss';
+
+const Profile: React.FC = memo(() => {
+  return <View className='test'>Profile</View>;
+});
+
+export default Profile;
